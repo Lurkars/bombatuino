48475c65e910a09aec99bd84a8bddb0e8256b81a
Lukas <lukas@haubaum.de>
2013-02-12 11:51:05 +0100

    4051 test sketch

--- sketches/testing/_4051_test.ino
+++ sketches/testing/_4051_test.ino
@@ -65,11 +65,12 @@ void loop() {
   if (pots[0] < read - tolerance || pots[0] > read + tolerance) {
     pots[0] = read;
     Serial.print("pot 0: ");
+    //print value between 0 & 127 like MIDI velocity ;)
     Serial.print(pots[0]/8);
     Serial.println();
   }  
   
-  //get Y1 of 4051
+  //get Y1 of 4051, analog to Y0
   digitalWrite(s0, HIGH);
   digitalWrite(s1, LOW);
   digitalWrite(s1, LOW);
